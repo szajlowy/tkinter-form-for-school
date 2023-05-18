9ca0c0b3085583fd7345a647d6736cd1a462f861
Dawid Tobaj <dawid.tobaj@zseitolsztyn.onmicrosoft.com>
2023-05-18 13:29:56 +0200

    First name entry

--- app.py
+++ app.py
@@ -4,11 +4,21 @@ window = tk.Tk()
 window.title('Form')
 window.geometry("600x800")
 window.resizable(False, False)
+window.grid_columnconfigure(0, weight=1)
+window.grid_columnconfigure(1, weight=3)
+
+
+first_name = tk.StringVar()
 
 
 label01 = tk.Label(window, text="Register", font='Verdana 16 bold')
-label01.grid(row=0, column=0)
-window.grid_columnconfigure(0, weight=1)
+label01.grid(row=0, column=0, sticky='we', columnspan=2)
+
+label02 = tk.Label(window, text='First name')
+label02.grid(row=1, column=0)
+
+entry01 = tk.Entry(window, textvariable=first_name)
+entry01.grid(row=1, column=1, sticky='ew')
 
 
 window.mainloop()

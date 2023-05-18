6240167da35abcdf64c21d50aea1b2b333e1fa7d
Dawid Tobaj <dawid.tobaj@zseitolsztyn.onmicrosoft.com>
2023-05-18 13:00:53 +0200

    Centered title

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ window.title('Form')
 window.geometry("600x800")
 window.resizable(False, False)
 
+
+label01 = tk.Label(window, text="Register", font='Verdana 16 bold')
+label01.grid(row=0, column=0, sticky='ew')
+window.grid_columnconfigure(0, weight=1)
+
+
 window.mainloop()
